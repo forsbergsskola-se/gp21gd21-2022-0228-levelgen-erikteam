bcfb75adaa9497e774a1ebe935cc449bf046e885
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-03-09 11:14:02 +0100

    Skillpopup script

---
+++ Assets/Olles Folder/ScriptsOR/SkillPopUp.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPopUp : MonoBehaviour
+{
+    public GameObject SkillPopUpPanel;
+    public void OnCollisionEnter(Collision collision)
+    {
+        SkillPopUpPanel.SetActive(true);
+    }
+}
