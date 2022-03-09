85ae81399c3a99f2334beb803e4c5a2e307cce37
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-03-09 12:58:09 +0100

    Update MonsterSpawner.cs

--- Assets/Emanoels Folder/Scripts/Enemy/MonsterSpawner.cs
+++ Assets/Emanoels Folder/Scripts/Enemy/MonsterSpawner.cs
@@ -14,7 +14,8 @@ public class MonsterSpawner : MonoBehaviour
         Debug.Log("entered square");
         for (int i = 0; i < monsterSpawner.Length; i++)
         {
-            Instantiate(monsterSpawner[i], new Vector3(10,10,10),Quaternion.identity);
+            monsterSpawner[i].gameObject.SetActive(true);
+            //Instantiate(monsterSpawner[i], new Vector3(10,10,10),Quaternion.identity);
             Debug.Log("Monster Spawned");
             this.gameObject.SetActive(false);
             Debug.Log("Is not Inactive");
