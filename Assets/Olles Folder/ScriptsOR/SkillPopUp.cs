2baf3522025584c876a4cb69a4da38ebdd434829
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-03-10 11:24:34 +0100

    merge

--- Assets/Olles Folder/ScriptsOR/SkillPopUp.cs
+++ Assets/Olles Folder/ScriptsOR/SkillPopUp.cs
@@ -1,14 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Multiplayer.Samples.BossRoom;
+using Unity.Multiplayer.Samples.BossRoom.Server;
 using UnityEngine;
 
 public class SkillPopUp : MonoBehaviour
 {
+    public ServerCharacter characterState;
     public GameObject SkillPopUpPanel;
-    public void OnCollisionEnter(Collision collision)
+    private int score = 0;
+
+
+    private void Awake()
     {
-        SkillPopUpPanel.SetActive(true);
-        this.gameObject.SetActive(false);
+        //score = 0;
+    }
+
+    private void OnLifeStateChanged(LifeState prevLifeState, LifeState lifeState)
+    {
+        if (lifeState != LifeState.Alive)
+        {
+          //  score++;
+        }
+    }
+
+    public void TriggerSkillPopUp()
+    {
+        for (int score = 0; score < score; score++)
+        {
+            if ((score % 20) == 0)
+            {
+
+            }
+        }
     }
 }
