96b579960bc00e156109aad2dad8e8380a18a5cc
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-03-11 07:59:39 +0100

    Room spawns when you are near, prefab fixes and such.

--- Assets/RoomTrigger.cs
+++ Assets/RoomTrigger.cs
@@ -3,81 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using Photon.Realtime;
 using UnityEngine;
-
+/// <summary>
+/// If you came this far into the code. Then OBOY are you in for a treat hahahaha
+/// </summary>
 public class RoomTrigger : MonoBehaviour
 {
     int currentCellx;
     int currentCellz;
-    private void Start()
-    {
-        currentCellx = 0;
-        currentCellz = 0;
-    }
-
+    private int oldCellx;
+    private int oldCellz;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "TriggerPoint")
         {
-            Debug.Log("COLLISION DETECTED");
-            if (other.gameObject.name.Contains("+Z"))
+            if (other.gameObject.transform.name.Contains("+Z"))
             {
-                Debug.Log("Xakzi is Awesome!Top");
                 currentCellz--;
-                other.GetComponent<Collider>().enabled = false;
+                other.gameObject.SetActive(false);
             }
-            if (other.gameObject.name.Contains("-Z"))
+            if (other.gameObject.transform.name.Contains("-Z"))
             {
-                Debug.Log("Xakzi is Awesome!Bottom");
                 currentCellz++;
-                other.GetComponent<Collider>().enabled = false;
+                other.gameObject.SetActive(false);
             }
-            if (other.gameObject.name.Contains("+X"))
+            if (other.gameObject.transform.name.Contains("+X"))
             {
-                Debug.Log("Xakzi is Awesome!Right");
                 currentCellx++;
-                other.GetComponent<Collider>().enabled = false;
+                other.gameObject.SetActive(false);
             }
-            if (other.gameObject.name.Contains("-X"))
+            if (other.gameObject.transform.name.Contains("-X"))
             {
-                Debug.Log("Xakzi is Awesome!Left");
                 currentCellx--;
-                other.GetComponent<Collider>().enabled = false;
+                other.gameObject.SetActive(false);
             }
 
+            var room2 = GameObject.FindGameObjectWithTag("Room"); // Room 0-0
+            var testroom = room2; //Room 0-0
+            string room3 = room2.name.Replace(room2.name, "Room " + currentCellx.ToString() + "-" + currentCellz.ToString()); // Room 0-1
 
-            //if(currentCellz == i && currentCellx == j)
-            //{
-            //generator.transform.FindChild(i.ToString()+"-"+j.ToString()).gameObject.SetActive(true);
+            var room4 = testroom.name.Replace("Room", "");
+            string[] subs = room4.Split('-');
 
-
-                var room2 = GameObject.FindGameObjectWithTag("Room");
-                string room3 = room2.name.Replace(room2.name, "Room " + currentCellx.ToString()+"-"+currentCellz.ToString());
-                Debug.Log($"New Room Name: {room3}, currentcellx: {currentCellx}, currentcellz: {currentCellz}");
-
-                var number = GameObject.Find(room3).transform.GetChild(0).childCount;
-
-                for (int i = 0; i < number; i++)
-                {
-                    GameObject.Find(room3).transform.GetChild(i).gameObject.SetActive(true);
-
-                }
-
-                other.enabled = false;
-
-                /*
-                 *
-                 * for (int i = 1; i < _board.Count; i++)
+            var number = GameObject.Find(room3).transform.childCount;
+            for (int i = 0; i < number; i++)
             {
-                count = this.transform.GetChild(i).childCount;
-                for (int j = 0; j < count; j++)
-                {
-                    this.transform.GetChild(i).transform.GetChild(j).gameObject.SetActive(false);
-                }
+                GameObject.Find(room3).transform.GetChild(i).gameObject.SetActive(true);
             }
-                 */
-                //}
 
+            if (oldCellx < currentCellx)
+            {
+                oldCellx = currentCellx;
+                oldCellz = currentCellz;
+
+                var findchild1 = GameObject.Find(room3);
+                var findchild2 = findchild1.transform.Find("exits");
+                var findchild3 = findchild2.transform.Find("exit door");
+                var findchild4 = findchild3.transform.Find("-X exit");
+                var findchild5 = findchild4.transform.Find("-X Trigger Point");
+                findchild5.transform.gameObject.SetActive(false);
+                var findchild6 = findchild4.transform.Find("+Z exit");
+                var findchild7 = findchild6.transform.Find("+Z Trigger Point");
+                findchild7.transform.gameObject.SetActive(true);
+                var findchild8 = findchild4.transform.Find("-Z exit");
+                var findchild9 = findchild8.transform.Find("-Z Trigger Point");
+                findchild9.transform.gameObject.SetActive(true);
+            }
+            if (oldCellx > currentCellx)
+            {
+                oldCellx = currentCellx;
+                oldCellz = currentCellz;
+
+                var findchild1 = GameObject.Find(room3);
+                var findchild2 = findchild1.transform.Find("exits");
+                var findchild3 = findchild2.transform.Find("exit door");
+                var findchild4 = findchild3.transform.Find("+X exit");
+                var findchild5 = findchild4.transform.Find("+X Trigger Point");
+                findchild5.transform.gameObject.SetActive(false);
+                var findchild6 = findchild4.transform.Find("+Z exit");
+                var findchild7 = findchild6.transform.Find("+Z Trigger Point");
+                findchild7.transform.gameObject.SetActive(true);
+                var findchild8 = findchild4.transform.Find("-Z exit");
+                var findchild9 = findchild8.transform.Find("-Z Trigger Point");
+                findchild9.transform.gameObject.SetActive(true);
+
+            }
+            if (oldCellz < currentCellz)
+            {
+                oldCellx = currentCellx;
+                oldCellz = currentCellz;
+
+                var findchild1 = GameObject.Find(room3);
+                var findchild2 = findchild1.transform.Find("exits");
+                var findchild3 = findchild2.transform.Find("exit door");
+                var findchild4 = findchild3.transform.Find("+Z exit");
+                var findchild5 = findchild4.transform.Find("+Z Trigger Point");
+                findchild5.transform.gameObject.SetActive(false);
+                var findchild6 = findchild4.transform.Find("+X exit");
+                var findchild7 = findchild6.transform.Find("+X Trigger Point");
+                findchild7.transform.gameObject.SetActive(true);
+                var findchild8 = findchild4.transform.Find("-X exit");
+                var findchild9 = findchild8.transform.Find("-X Trigger Point");
+                findchild9.transform.gameObject.SetActive(true);
+            }
+            if (oldCellz > currentCellz)
+            {
+                oldCellx = currentCellx;
+                oldCellz = currentCellz;
+
+                var findchild1 = GameObject.Find(room3);
+                var findchild2 = findchild1.transform.Find("exits");
+                var findchild3 = findchild2.transform.Find("exit door");
+                var findchild4 = findchild3.transform.Find("-Z exit");
+                var findchild5 = findchild4.transform.Find("-Z Trigger Point");
+                findchild5.transform.gameObject.SetActive(false);
+                var findchild6 = findchild4.transform.Find("+X exit");
+                var findchild7 = findchild6.transform.Find("+X Trigger Point");
+                findchild7.transform.gameObject.SetActive(true);
+                var findchild8 = findchild4.transform.Find("-X exit");
+                var findchild9 = findchild8.transform.Find("-X Trigger Point");
+                findchild9.transform.gameObject.SetActive(true);
+            }
         }
     }
 }
